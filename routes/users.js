54f13ad7252674afeacb4d577f7652b9ae005500
wu-ailsa <ailsawunyc@gmail.com>
2023-11-08 22:42:31 -0500

    rendered static files

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const router = express.Router()
 
+router.use(logger)
+
 router.get('/',(req,res)=>{
+    console.log(req.query.name)
     res.send('User List')
 })
 
 router.get('/new',(req,res)=>{
-    res.send('User New Form')
+    res.render("users/new",{firstName: "" })
 })
 
 router.post('/',(req,res)=>{
-    res.send('Create User')
+    const isValid = false
+    if (isValid) {
+        users.push({firstName: req.body.firstName})
+        res.redirect(`/users/${users.length -1}`)
+    } else {
+        console.log("Error")
+        res.render('users/new', {firstName: req.body.firstName})
+    }
+    console.log(req.body.firstName)
+    res.send('Hi! Welcome!')
 })
 
 //router
@@ -32,5 +44,10 @@ router.param("id",(req,res,next,id)=>{
     req.user = users[id]
     next()
 })
+
+function logger(req, res, next){
+    console.log(req.originalUrl)
+    next()
+}
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 
 app.set('view engine','ejs');
-app.use(logger)
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
 
-app.get('/',(req,res) =>{
+app.get('/', (req,res) =>{
     //res.download('img/anderson-rangel-sky.jpg')
     res.render("index",{text: "Sample text response: World"})
 });
@@ -15,9 +17,6 @@ const userRouter = require('./routes/users')
 app.use('/users',userRouter)
 // app.use('/posts',postRouter)
 
-function logger(req, res, next){
-    console.log(req.originalUrl)
-    next()
-}
+
 
 app.listen(3000);
