42c0f40584bdd3a91699aac35569cd099f3a518b
wu-ailsa <ailsawunyc@gmail.com>
2023-11-08 21:15:02 -0500

    created router in users.js

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ app.set('view engine','ejs');
 app.get('/',(req,res) =>{
     console.log("Here");
     // res.download('img/anderson-rangel-sky.jpg')
-    res.render('index')
+    res.render("index",{text: "Sample text response: World"})
 });
+
+const userRouter = require('./routes/users')
+// const postRouter = require('./routes/posts')
+
+app.use('/users',userRouter)
+// app.use('/posts',postRouter)
 
 app.listen(3000);
